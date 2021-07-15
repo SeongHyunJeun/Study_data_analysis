d85ff9f29e5af2413faf21fcf2647b5935767725
seonghyun <esdzx5639@naver.com>
2021-07-15 21:51:37 +0900

    Initial commit

---
+++ temperature/main.py
@@ -0,0 +1,22 @@
+import csv
+
+max_temp = 30
+max_date = ''
+
+f = open('seoul.csv')
+data = csv.reader(f)
+header = next(data)
+
+for row in data:
+    if (row[-1] == ''):
+        row[-1] = -999
+
+    row[-1] = float(row[-1])
+
+    if (row[-1] > max_temp):
+        max_temp = row[-1]
+        max_date = row[0]
+
+f.close()
+
+print('서울의 가장 더웠던 날은', max_date, '일, ', max_temp, '도 입니다.')
